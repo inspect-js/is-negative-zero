09e2e537390225c1d1a6912be64267eaec6ea367
Jordan Harband <ljharb@gmail.com>
2020-01-28 22:15:15 -0800

    [Dev Deps] update `eslint`, `@ljharb/eslint-config`, `tape`; add `safe-publish-latest`

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('tape');
-var isNegativeZero = require('./');
+var isNegativeZero = require('../');
 
 test('not negative zero', function (t) {
 	t.notOk(isNegativeZero(), 'undefined is not negative zero');
